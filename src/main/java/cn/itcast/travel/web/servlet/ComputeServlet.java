4d443b3df69a314e072ceef15e7af32d95a87605
小康康 <2433964464@qq.com>
2020-02-01 16:59:30 +0800

    增加了加法和乘法，去掉了input步骤

--- src/main/java/cn/itcast/travel/web/servlet/ComputeServlet.java
+++ src/main/java/cn/itcast/travel/web/servlet/ComputeServlet.java
@@ -2,70 +2,82 @@ package cn.itcast.travel.web.servlet;
 
 import cn.itcast.travel.domain.Matrix;
 import cn.itcast.travel.domain.ResultInfo;
-import cn.itcast.travel.util.ComputeUtils;
+import cn.itcast.travel.service.impl.ComputeServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.beanutils.BeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
 
-@WebServlet("/computeServlet")
-public class ComputeServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        1.获取用二维数组
-//        System.out.println(">>>>>>>>>>>--");
-//        Map<String,String[]> map = request.getParameterMap();
+@WebServlet("/computeServlet/*")
+public class ComputeServlet extends BaseServlet {
+
+    ComputeServiceImpl computeServiceImpl = new ComputeServiceImpl();
+
+
+    public void matrixAdd(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         String row = request.getParameter("row");
-        System.out.println("row:"+row);
+        System.out.println("row:" + row);
         String col = request.getParameter("col");
-        System.out.println("col"+col);
+        System.out.println("col" + col);
         String Date = request.getParameter("Date");
-        System.out.println("Date:"+Date);
-        if (row!=null&&col!=null&&Date!=null) {
-            Matrix matrix = new Matrix(Integer.parseInt(row), Integer.parseInt(col), ComputeUtils.getArray(row, col, Date));
-            System.out.println(matrix.toString());
-            System.out.println(matrix.getValue().length);
+        System.out.println("Date"+Date);
+        String Date2 = request.getParameter("Date2");
+        System.out.println("Date2"+Date2);
+//        System.out.println("Date:" + Date);
+//        if (row!=null&&col!=null&&Date!=null) {
+//            Matrix matrix = new Matrix(Integer.parseInt(row), Integer.parseInt(col), ComputeServiceImpl.getArray(row, col, Date));
+//            System.out.println(matrix.toString());
+//            System.out.println(matrix.getValue().length);
+//        }
+//        if (row!=null&&col!=null&&Date2!=null) {
+//            Matrix matrix2 = new Matrix(Integer.parseInt(row), Integer.parseInt(col), ComputeServiceImpl.getArray(row, col, Date2));
+//        }
+
+        ResultInfo info = new ResultInfo();
+
+        if (row != null && col != null && Date != null && Date2 != null) {
+            Matrix matrix = new Matrix(Integer.parseInt(row), Integer.parseInt(col), ComputeServiceImpl.getArray(row, col, Date));
+            Matrix matrix2 = new Matrix(Integer.parseInt(row), Integer.parseInt(col), ComputeServiceImpl.getArray(row, col, Date2));
+            Matrix matrix3 = computeServiceImpl.matrixAdd(matrix, matrix2);
+            matrix3.toString();
+            info.setFlag(true);
+            info.setData(matrix3);
+            info.setErrorMsg("计算成功！");;
+        } else {
+//            返回提示：无法计算，有空值
+            info.setFlag(false);
+            info.setErrorMsg("无法计算！");
+            System.out.println("shibai");
+        }
+        //        将info对象序列化
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(info);
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(json);
+
+    }
+    public void matrixMultiply(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String row = request.getParameter("row");
+        System.out.println("row:" + row);
+        String col = request.getParameter("col");
+        System.out.println("col" + col);
+        String Date = request.getParameter("Date");
+        String Date2 = request.getParameter("Date2");
+        if (row != null && col != null && Date != null && Date2 != null) {
+
+            Matrix matrix = new Matrix(Integer.parseInt(row), Integer.parseInt(col), ComputeServiceImpl.getArray(row, col, Date));
+            Matrix matrix2 = new Matrix(Integer.parseInt(row), Integer.parseInt(col), ComputeServiceImpl.getArray(row, col, Date2));
+            Matrix matrix3 = computeServiceImpl.matrixMultiply(matrix, matrix2);
+//            return matrix3;
+        } else {
+//            返回提示：无法计算，有空值
+
 
         }
-
-
-//        2.封装Matrix对象
-//        Matrix matrix = new Matrix();
-//        try {
-//            BeanUtils.populate(matrix,map);
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        } catch (InvocationTargetException e) {
-//            e.printStackTrace();
-//        }
-//        //        3.调用计算工具计算
-//        ComputeUtils  computeUtils = new ComputeUtils();
-//        Matrix m =computeUtils.inputMatrix(matrix);
-//
-//        ResultInfo info = new ResultInfo();
-//
-////        4.判断用户对象是否为null
-//        if (m != null){
-////            矩阵插入成功
-//            info.setFlag(true);
-//            info.setErrorMsg("矩阵插入成功");
-//        }else{
-//            //            矩阵插入失败
-//            info.setFlag(false);
-//            info.setErrorMsg("矩阵插入失败");
-//        }
-//
-////        响应数据
-//        ObjectMapper mapper = new ObjectMapper();
-//
-//        response.setContentType("application/json;charset=utf-8");
-//        mapper.writeValue(response.getOutputStream(),info);
 
     }
 
